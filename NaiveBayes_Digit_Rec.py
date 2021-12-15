fc7336b2b039803554772a9263885d94fec2d0e5
ibbykhazanchi <khajanchi.ibrahim@gmail.com>
2021-12-14 22:57:40 -0500

    added knn algo file

--- NaiveBayes_Digit_Rec.py
+++ NaiveBayes_Digit_Rec.py
@@ -1,30 +1,33 @@
 import math
 import random
-#Read Global Files
-#read training data into list
-faceFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\trainingimages",'r')
+
+# Read Global Files
+# read training data into list
+faceFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\trainingimages", "r")
 train_lines = faceFile.readlines()
 faceFile.close()
-#read training data labels into a list
-labelFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\traininglabels",'r')
+# read training data labels into a list
+labelFile = open(
+    r"C:\Users\atfan\github\AI_Final_Project\digitdata\traininglabels", "r"
+)
 train_labels = []
 for c in labelFile.read():
-    if c != '\n' and c != ' ':
+    if c != "\n" and c != " ":
         train_labels.append(c)
 labelFile.close()
-#read testing data into list
-faceFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\testimages",'r')
+# read testing data into list
+faceFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\testimages", "r")
 test_lines = faceFile.readlines()
 faceFile.close()
-#read testing data labels into a list
-labelFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\testlabels",'r')
+# read testing data labels into a list
+labelFile = open(r"C:\Users\atfan\github\AI_Final_Project\digitdata\testlabels", "r")
 test_labels = []
 for c in labelFile.read():
-    if c != '\n' and c != ' ':
+    if c != "\n" and c != " ":
         test_labels.append(c)
 labelFile.close()
 
-#Global Variables
+# Global Variables
 ITERATIONS = 5
 DATA_PERCENT = 1
 SAMPLE_TOTAL = len(train_labels)
@@ -34,10 +37,10 @@ FEATURE_COLS = 28
 FEATURE_TOTAL = FEATURE_ROWS * FEATURE_COLS
 IMAGE_PIX_WIDTH = 28
 IMAGE_PIX_HEIGHT = 28
-PIX_INCREMENT_W = IMAGE_PIX_WIDTH//FEATURE_ROWS
-PIX_INCREMENT_H = IMAGE_PIX_HEIGHT//FEATURE_COLS
+PIX_INCREMENT_W = IMAGE_PIX_WIDTH // FEATURE_ROWS
+PIX_INCREMENT_H = IMAGE_PIX_HEIGHT // FEATURE_COLS
 
-#class to get 2-D list of features for every instance
+# class to get 2-D list of features for every instance
 class Features:
     def __init__(self, f_rows, f_cols, pix_incr_W, pix_incr_H, d_total, lines):
         self.data_total = d_total
@@ -57,11 +60,11 @@ class Features:
             endFeaty = self.pixel_incr_W
             for cols in range(self.f_cols):
                 count = 0
-                for line in self.lines[self.startFeatx:self.endFeatx]:
+                for line in self.lines[self.startFeatx : self.endFeatx]:
                     startFeaty = startFeaty
                     endFeaty = endFeaty
                     for char in line[startFeaty:endFeaty]:
-                        if char == '#' or char == '+':
+                        if char == "#" or char == "+":
                             count += 1
                 f_list.append(count)
                 startFeaty += self.pixel_incr_W
@@ -69,7 +72,8 @@ class Features:
                 numFeat += 1
             self.startFeatx += self.pixel_incr_H
             self.endFeatx += self.pixel_incr_H
-    #returns a 2D list of all feature values across all instances in a data file
+
+    # returns a 2D list of all feature values across all instances in a data file
     def get_f_list(self):
         for x in range(self.data_total):
             featList = []
@@ -77,7 +81,8 @@ class Features:
             self.f_list.append(featList)
         return self.f_list
 
-#count number of instances that are either true or false
+
+# count number of instances that are either true or false
 def get_digit_totals(labels_list, d_total):
     count_0 = 0
     count_1 = 0
@@ -90,32 +95,44 @@ def get_digit_totals(labels_list, d_total):
     count_8 = 0
     count_9 = 0
     for x in range(d_total):
-        if labels_list[x] == '0':
+        if labels_list[x] == "0":
             count_0 += 1
-        elif labels_list[x] == '1':
+        elif labels_list[x] == "1":
             count_1 += 1
-        elif labels_list[x] == '2':
+        elif labels_list[x] == "2":
             count_2 += 1
-        elif labels_list[x] == '3':
+        elif labels_list[x] == "3":
             count_3 += 1
-        elif labels_list[x] == '4':
+        elif labels_list[x] == "4":
             count_4 += 1
-        elif labels_list[x] == '5':
+        elif labels_list[x] == "5":
             count_5 += 1
-        elif labels_list[x] == '6':
+        elif labels_list[x] == "6":
             count_6 += 1
-        elif labels_list[x] == '7':
+        elif labels_list[x] == "7":
             count_7 += 1
-        elif labels_list[x] == '8':
+        elif labels_list[x] == "8":
             count_8 += 1
-        elif labels_list[x] == '9':
+        elif labels_list[x] == "9":
             count_9 += 1
         else:
             continue
 
-    return count_0, count_1, count_2, count_3, count_4, count_5, count_6, count_7, count_8, count_9
+    return (
+        count_0,
+        count_1,
+        count_2,
+        count_3,
+        count_4,
+        count_5,
+        count_6,
+        count_7,
+        count_8,
+        count_9,
+    )
 
-#return a tuple of 2_D lists that contain data tables for instance label = true and label = false
+
+# return a tuple of 2_D lists that contain data tables for instance label = true and label = false
 def get_data_tables(f_list, labels_list, feat_total, d_total):
     data_table_0 = []
     data_table_1 = []
@@ -139,25 +156,25 @@ def get_data_tables(f_list, labels_list, feat_total, d_total):
         data_rows_8 = []
         data_rows_9 = []
         for x in range(d_total):
-            if labels_list[x] == '0':
+            if labels_list[x] == "0":
                 data_rows_0.append(f_list[x][y])
-            elif labels_list[x] == '1':
+            elif labels_list[x] == "1":
                 data_rows_1.append(f_list[x][y])
-            elif labels_list[x] == '2':
+            elif labels_list[x] == "2":
                 data_rows_2.append(f_list[x][y])
-            elif labels_list[x] == '3':
+            elif labels_list[x] == "3":
                 data_rows_3.append(f_list[x][y])
-            elif labels_list[x] == '4':
+            elif labels_list[x] == "4":
                 data_rows_4.append(f_list[x][y])
-            elif labels_list[x] == '5':
+            elif labels_list[x] == "5":
                 data_rows_5.append(f_list[x][y])
-            elif labels_list[x] == '6':
+            elif labels_list[x] == "6":
                 data_rows_6.append(f_list[x][y])
-            elif labels_list[x] == '7':
+            elif labels_list[x] == "7":
                 data_rows_7.append(f_list[x][y])
-            elif labels_list[x] == '8':
+            elif labels_list[x] == "8":
                 data_rows_8.append(f_list[x][y])
-            elif labels_list[x] == '9':
+            elif labels_list[x] == "9":
                 data_rows_9.append(f_list[x][y])
             else:
                 continue
@@ -173,9 +190,21 @@ def get_data_tables(f_list, labels_list, feat_total, d_total):
         data_table_8.append(data_rows_8)
         data_table_9.append(data_rows_9)
 
-    return data_table_0, data_table_1, data_table_2, data_table_3, data_table_4, data_table_5, data_table_6, data_table_7, data_table_8, data_table_9
+    return (
+        data_table_0,
+        data_table_1,
+        data_table_2,
+        data_table_3,
+        data_table_4,
+        data_table_5,
+        data_table_6,
+        data_table_7,
+        data_table_8,
+        data_table_9,
+    )
 
-#returns integer that represents the max possible value a data point can take in the data tables
+
+# returns integer that represents the max possible value a data point can take in the data tables
 def get_max_feat_value(data_tables):
     max_list = []
     for y in range(10):
@@ -185,7 +214,8 @@ def get_max_feat_value(data_tables):
     max_f_value = max(max_list)
     return max_f_value
 
-#return a list of computed probabilities for each data point occuring in each feature out of all instances
+
+# return a list of computed probabilities for each data point occuring in each feature out of all instances
 def get_data_feature_probs(feat_total, max_val, data_tables, tf_totals):
     data_probs = []
     for i in range(feat_total):
@@ -196,28 +226,41 @@ def get_data_feature_probs(feat_total, max_val, data_tables, tf_totals):
                 if j == num:
                     num_count += 1
             if num_count != 0:
-                data_count.append(num_count/tf_totals)
+                data_count.append(num_count / tf_totals)
             else:
                 data_count.append(0.0001)
 
         data_probs.append(data_count)
     return data_probs
 
-#returns a tuple of probabilities representing the occurance of a true instance and a false instance
-def prob_instance_prior(d_total, dig_total):
-    prob_0 = dig_total[0]/d_total
-    prob_1 = dig_total[1]/d_total
-    prob_2 = dig_total[2]/d_total
-    prob_3 = dig_total[3]/d_total
-    prob_4 = dig_total[4]/d_total
-    prob_5 = dig_total[5]/d_total
-    prob_6 = dig_total[6]/d_total
-    prob_7 = dig_total[7]/d_total
-    prob_8 = dig_total[8]/d_total
-    prob_9 = dig_total[9]/d_total
-    return prob_0, prob_1, prob_2, prob_3, prob_4, prob_5, prob_6, prob_7, prob_8, prob_9
 
-#return a list of "1"'s and "0"'s (representing true or false) that Naive Bayes determined based on training data for each instance
+# returns a tuple of probabilities representing the occurance of a true instance and a false instance
+def prob_instance_prior(d_total, dig_total):
+    prob_0 = dig_total[0] / d_total
+    prob_1 = dig_total[1] / d_total
+    prob_2 = dig_total[2] / d_total
+    prob_3 = dig_total[3] / d_total
+    prob_4 = dig_total[4] / d_total
+    prob_5 = dig_total[5] / d_total
+    prob_6 = dig_total[6] / d_total
+    prob_7 = dig_total[7] / d_total
+    prob_8 = dig_total[8] / d_total
+    prob_9 = dig_total[9] / d_total
+    return (
+        prob_0,
+        prob_1,
+        prob_2,
+        prob_3,
+        prob_4,
+        prob_5,
+        prob_6,
+        prob_7,
+        prob_8,
+        prob_9,
+    )
+
+
+# return a list of "1"'s and "0"'s (representing true or false) that Naive Bayes determined based on training data for each instance
 def get_results(d_total, feat_total, max_val, f_list, data_probs, prob_prior):
     results_list = []
     for z in range(d_total):
@@ -228,11 +271,11 @@ def get_results(d_total, feat_total, max_val, f_list, data_probs, prob_prior):
                 for check in range(max_val):
                     if f_list[z][n] == check:
                         feat_prob_sum += math.log(data_probs[dig][n][check])
-            probs_list.append(feat_prob_sum*prob_prior[dig])
+            probs_list.append(feat_prob_sum * prob_prior[dig])
 
         check_max = probs_list[0]
         max_index = 0
-        for x in range(1,10):
+        for x in range(1, 10):
             if probs_list[x] > check_max:
                 check_max = probs_list[x]
                 max_index = x
@@ -240,9 +283,10 @@ def get_results(d_total, feat_total, max_val, f_list, data_probs, prob_prior):
 
     return results_list
 
-#compare results list to actual label list and compute the accuracy of the results, then print the accuracy
+
+# compare results list to actual label list and compute the accuracy of the results, then print the accuracy
 def print_accuracy(labels_list, d_total, r_list):
-    #convert string list to integers
+    # convert string list to integers
     for i in range(d_total):
         labels_list[i] = int(labels_list[i])
 
@@ -251,18 +295,19 @@ def print_accuracy(labels_list, d_total, r_list):
         if r_list[val] == labels_list[val]:
             correct += 1
 
-    accuracy = correct/d_total
+    accuracy = correct / d_total
 
-    print('accuracy: ',round(accuracy*100,2),'%')
+    print("accuracy: ", round(accuracy * 100, 2), "%")
 
     return accuracy
 
-#main function to initialize all other functions
+
+# main function to initialize all other functions
 def main(percent):
-    #set sample size based on a percentage of total data set
-    TRAIN_DATA_TOTAL = int(round(len(train_labels)*percent, 0))
+    # set sample size based on a percentage of total data set
+    TRAIN_DATA_TOTAL = int(round(len(train_labels) * percent, 0))
     # print(TRAIN_DATA_TOTAL)
-    #match labels with images in a list
+    # match labels with images in a list
     start = 0
     end = 28
     train_img_labels = []
@@ -270,50 +315,81 @@ def main(percent):
         image = []
         lab_img = []
         for line in train_lines[start:end]:
-                image.append(line)
+            image.append(line)
         lab_img.append(image)
         lab_img.append(train_labels[x])
         train_img_labels.append(lab_img)
-        start+=28
-        end+=28
-    #randomize data based on sample size
+        start += 28
+        end += 28
+    # randomize data based on sample size
     rand_train_img_label = random.sample(train_img_labels, TRAIN_DATA_TOTAL)
-    #seperate images and labels into distinct lists
+    # seperate images and labels into distinct lists
     new_train_img = []
     new_train_labels = []
     for i in rand_train_img_label:
         for j in i[0]:
             new_train_img.append(j)
         new_train_labels.append(i[1])
-    #Initialize classes to get feature lists from training and testing
-    features = Features(FEATURE_ROWS, FEATURE_COLS, PIX_INCREMENT_W, PIX_INCREMENT_H, TRAIN_DATA_TOTAL, new_train_img)
+    # Initialize classes to get feature lists from training and testing
+    features = Features(
+        FEATURE_ROWS,
+        FEATURE_COLS,
+        PIX_INCREMENT_W,
+        PIX_INCREMENT_H,
+        TRAIN_DATA_TOTAL,
+        new_train_img,
+    )
     train_feat_list = features.get_f_list()
-    features = Features(FEATURE_ROWS, FEATURE_COLS, PIX_INCREMENT_W, PIX_INCREMENT_H, TEST_DATA_TOTAL, test_lines)
+    features = Features(
+        FEATURE_ROWS,
+        FEATURE_COLS,
+        PIX_INCREMENT_W,
+        PIX_INCREMENT_H,
+        TEST_DATA_TOTAL,
+        test_lines,
+    )
     test_feat_list = features.get_f_list()
-    #Initialize training functions
+    # Initialize training functions
     digit_totals = get_digit_totals(new_train_labels, TRAIN_DATA_TOTAL)
-    digit_data_tables = get_data_tables(train_feat_list, new_train_labels, FEATURE_TOTAL, TRAIN_DATA_TOTAL)
+    digit_data_tables = get_data_tables(
+        train_feat_list, new_train_labels, FEATURE_TOTAL, TRAIN_DATA_TOTAL
+    )
     max_feature_value = get_max_feat_value(digit_data_tables)
     digit_probs_list = []
     for p in range(10):
-        digit_probs_list.append(get_data_feature_probs(FEATURE_TOTAL, max_feature_value+1, digit_data_tables[p], digit_totals[p]))
+        digit_probs_list.append(
+            get_data_feature_probs(
+                FEATURE_TOTAL,
+                max_feature_value + 1,
+                digit_data_tables[p],
+                digit_totals[p],
+            )
+        )
     prob_inst_prior = prob_instance_prior(TRAIN_DATA_TOTAL, digit_totals)
-    #Initialize testing functions
-    result_list = get_results(TEST_DATA_TOTAL, FEATURE_TOTAL, max_feature_value+1, test_feat_list, digit_probs_list, prob_inst_prior)
+    # Initialize testing functions
+    result_list = get_results(
+        TEST_DATA_TOTAL,
+        FEATURE_TOTAL,
+        max_feature_value + 1,
+        test_feat_list,
+        digit_probs_list,
+        prob_inst_prior,
+    )
     acc = print_accuracy(test_labels, TEST_DATA_TOTAL, result_list)
 
-    return round(acc*100,2)
+    return round(acc * 100, 2)
 
-#init main
+
+# init main
 acc_list = []
 for x in range(ITERATIONS):
     acc_list.append(main(DATA_PERCENT))
-mean_acc = sum(acc_list)/len(acc_list)
+mean_acc = sum(acc_list) / len(acc_list)
 var_mean = 0
 for i in acc_list:
-    var_mean += pow((i-mean_acc),2)
-var = var_mean/len(acc_list)
+    var_mean += pow((i - mean_acc), 2)
+var = var_mean / len(acc_list)
 std_dev = math.sqrt(var)
-print('mean accuracy: ',round(mean_acc,2),'%')
-print('variance: ', round(var,2))
-print('standard deviation: ',round(std_dev,3))
+print("mean accuracy: ", round(mean_acc, 2), "%")
+print("variance: ", round(var, 2))
+print("standard deviation: ", round(std_dev, 3))
---
+++ knn.py
@@ -0,0 +1,202 @@
+import random
+from typing import DefaultDict, Text
+import numpy as np
+import timeit
+import math
+import statistics
+
+# globals
+FACE_BLOCK_LEN = 10
+
+
+def viewAsBlocks(arr, BSZ):
+    # arr is input array, BSZ is block-size
+    m, n = arr.shape
+    M, N = BSZ
+    return arr.reshape(m // M, M, n // N, N).swapaxes(1, 2).reshape(-1, M, N)
+
+
+def computeFeature(block):
+    count = 0
+    totalCount = 0
+    for row in block:
+        for element in row:
+            totalCount += 1
+            if element == 1:
+                count += 1
+
+    return count / totalCount
+
+
+def matrixify(arr, mode):
+    width = 0
+    if mode == 1:
+        width = 28
+    else:
+        width = 60
+    numberImg = []
+    for line in arr:
+        for i in range(width):
+            if not line[i].strip():
+                numberImg.append(0)
+            else:
+                numberImg.append(1)
+
+    matrix = np.array(numberImg)
+    if mode == 0:
+        matrix = matrix.reshape(70, 60)
+
+    return matrix
+
+
+def euclideanDistance(img1, img2, mode):
+    # if mode == 0 extract features
+    if mode == 0:
+        mat1 = viewAsBlocks(img1, (FACE_BLOCK_LEN, FACE_BLOCK_LEN))
+        mat2 = viewAsBlocks(img2, (FACE_BLOCK_LEN, FACE_BLOCK_LEN))
+        features1 = [computeFeature(block) for block in mat1]
+        features2 = [computeFeature(block) for block in mat2]
+        currSum = 0
+
+        for f1, f2 in zip(features1, features2):
+            squaredDifference = (f1 - f2) ** 2
+            currSum += squaredDifference
+        return math.sqrt(currSum)
+    else:
+        return sum((img1 - img2) ** 2)
+
+
+def majorityLabel(labels):
+    counterDict = DefaultDict(int)
+    for label in labels:
+        counterDict[label] += 1
+
+    majorityLabel = max(counterDict.values())
+    for key, value in counterDict.items():
+        if value == majorityLabel:
+            return key
+
+
+def predict(k, trainingImages, trainingLabels, testImage, mode):
+    distances = [
+        (euclideanDistance(testImage, image, mode), label)
+        for (image, label) in zip(trainingImages, trainingLabels)
+    ]
+
+    # sort
+    sortedDistances = sorted(distances, key=lambda tup: tup[0])
+    # for tup in by_distances:
+    # print(tup)
+
+    # get k closest labels
+    kClosest = [label for (_, label) in sortedDistances[:k]]
+
+    res = majorityLabel(kClosest)
+    # print(res)
+    return res
+
+
+def getImages(lines, labels, mode):
+    images = []
+    intlabels = []
+    width = 0
+
+    if mode == 1:
+        length = 28
+    else:
+        length = 70
+
+    # create matrix-label tuples and store in list
+    left = 0
+    right = length
+    count = 0
+
+    while right <= len(lines):
+        # add matrix label tuples
+        images.append((matrixify(lines[left:right], mode)))
+        intlabels.append(int(labels[count]))
+        left = right
+        right = right + length
+        count += 1
+
+    return images, intlabels
+
+
+def main(mode):
+    start = timeit.default_timer()
+
+    testImagesPath = ""
+    testLabelsPath = ""
+    trainImagesPath = ""
+    trainLabelsPath = ""
+
+    if mode == 1:  # digits
+        print("CLASSIFYING DIGITS")
+        testImagesPath = "digitdata/testimages"
+        testLabelsPath = "digitdata/testlabels"
+        trainImagesPath = "digitdata/trainingimages"
+        trainLabelsPath = "digitdata/traininglabels"
+    else:  # face
+        print("CLASSIFYING FACES")
+        testImagesPath = "facedata/facedatatest"
+        testLabelsPath = "facedata/facedatatestlabels"
+        trainImagesPath = "facedata/facedatatrain"
+        trainLabelsPath = "facedata/facedatatrainlabels"
+
+    # read the testing numbers into list
+    testFile = open("data/{}".format(testImagesPath), "r")
+    testLines = testFile.readlines()
+    testFile.close()
+
+    # read testing labels for digits into list
+    testLabelFile = open("data/{}".format(testLabelsPath), "r")
+    testLabelLines = testLabelFile.readlines()
+    testLabelFile.close()
+
+    # read the training numbers into list
+    digitFile = open("data/{}".format(trainImagesPath), "r")
+    digitLines = digitFile.readlines()
+    digitFile.close()
+
+    # read training labels for digits into list
+    labelFile = open("data/{}".format(trainLabelsPath), "r")
+    labelLines = labelFile.readlines()
+    labelFile.close()
+
+    testingImages, testingLabels = getImages(testLines, testLabelLines, mode)
+    trainingImages, trainingLabels = getImages(digitLines, labelLines, mode)
+
+    trainDataAmount = int(round(0.1 * len(trainingImages)))
+    accuracies = []
+    print(len(trainingImages[:trainDataAmount]))
+    # randomize testing images
+    randomizedImages = [tup for tup in zip(testingImages, testingLabels)]
+    randomizedImages = random.sample(randomizedImages, len(randomizedImages))
+
+    print(len(randomizedImages))
+
+    numCorrect = 0
+    i = 0
+    for j in range(5):
+        for testImage, testLabel in randomizedImages[:500]:
+            prediction = predict(
+                10,
+                trainingImages[:trainDataAmount],
+                trainingLabels[:trainDataAmount],
+                testImage,
+                mode,
+            )
+            if prediction == testLabel:
+                numCorrect += 1
+            i += 1
+
+        accuracies.append(numCorrect / 500)
+        print(accuracies[0])
+
+    # print(sum(accuracies) / len(accuracies))
+    print(statistics.variance(accuracies))
+    stop = timeit.default_timer()
+    print(stop - start)
+
+
+main(1)
